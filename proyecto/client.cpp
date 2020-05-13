c592ee4631282a5fcee62d66af5fff3772fd4372
Isacc Zúñiga <isaack9734@gmail.com>
2020-05-13 17:43:53 -0600

    cliente funciona con archivos html

--- proyecto/client.cpp
+++ proyecto/client.cpp
@@ -13,8 +13,6 @@
 int main( int argc, char * argv[] ) {
 
    Socket s( 's', false);
-   
-   
    char* request = argv[1]; 
    char* ip_address = (char*)calloc(20, sizeof(char)); 
    
@@ -24,14 +22,28 @@ int main( int argc, char * argv[] ) {
    char* filename = (char*)calloc(20, sizeof(char)); 
    
    int len_request = strlen(request); 
+   char* port = (char*)calloc(20, sizeof(char)); 
+    int server_port = 0; 
    
    
    while (!found_end) {
-	   if (*(request+counter) != '/') {
+	   if (*(request+counter) != '/' && *(request+counter) != ':') {
 			*(ip_address+counter) = *(request+counter); 
 			++counter;
 		}
 		else {
+			
+			if (*(request+counter) == ':') {
+				int counter_port = 0; 
+				++counter; 
+				while(*(request+counter) != '/') {
+					*(port+counter_port) = *(request+counter);
+					++counter; 
+					++counter_port;  
+					server_port = 1; 
+				}
+			}
+			
 			found_end = true; 
 			++counter; 
 			int counter_filename = 0; 
@@ -42,10 +54,23 @@ int main( int argc, char * argv[] ) {
 			}
 		} 
 	}
+	
+	
+	if (server_port) {
+		server_port = atoi(port); 
+	}
+	else {
+		server_port = SERVER_PORT;
+	}
    
    
     int id_file; 
-	char* file = (char*)"index.html"; 
+    
+    
+    
+	char* file = (char*)calloc(20, sizeof(char)); 
+   
+	strcpy(file, filename); 
    
    	id_file = open(file, O_RDWR | O_CREAT | S_IRUSR | S_IWUSR);
 
@@ -53,9 +78,7 @@ int main( int argc, char * argv[] ) {
 
    request = make_request_header(filename);
 
-	printf("RE: %s", request); 
-
-   int status_connect = s.Connect(ip_address, SERVER_PORT );
+   int status_connect = s.Connect(ip_address, server_port );
 
    int write_status = s.Write(request);
 
@@ -66,32 +89,25 @@ int main( int argc, char * argv[] ) {
 	int start_data = 0; 
 	int current_len = 0; 
  
+ 
 	while((read_status = s.Read(response, 512)) > 0) {
-		printf("entre"); 
 		if (bytes_read != 0) {
-			current_len = strlen(response); 
-			bytes_read+= current_len; 	
-			write(id_file, response, current_len);
+			bytes_read+= read_status; 	
+			write(id_file, response, read_status);
 			memset(response, 0, 512 * sizeof(char)); 	
 		}
-		else {		 
-			/*
+		else {
 				int counter_data = 0; 
-				int len_response = strlen(response); 
-				while(counter_data < len_response) {	 
-					if (response[counter_data] == '\\' && response[counter_data+1] == 'n' && response[counter_data+2] == '\\' && response[counter_data+3] == 'n') {
-						start_data = counter_data+4;  
-						counter_data = len_response;
+				while(counter_data < read_status) {	 
+					if (response[counter_data] == '\r' && response[counter_data+1] == '\n' && response[counter_data+2] == '\r' && response[counter_data+3] == '\n') { 
+						start_data = counter_data+3;  
+						counter_data = read_status;
 					}
 					++counter_data; 
-				} 
-			write(id_file, response+start_data, (len_response - start_data)); 		
-		*/
-
-			current_len = strlen(response); 	
-			bytes_read+= current_len; 
-			write(id_file, response, current_len); 	
-			memset(response, 0, 512 * sizeof(char));	
+				}
+			int diference = read_status - start_data; 	 
+			write(id_file, response+start_data, diference); 		
+			
 		}
 
 	}
