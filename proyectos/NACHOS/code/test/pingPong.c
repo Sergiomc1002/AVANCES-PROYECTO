d907c1c96d8fa79e4e1ed57ce29e1084a7848198
Isacc Zúñiga <isaack9734@gmail.com>
2020-06-19 16:14:13 -0600

    pingPong arreglado

--- proyectos/NACHOS/code/test/pingPong.c
+++ proyectos/NACHOS/code/test/pingPong.c
@@ -1,10 +1,9 @@
 #include "syscall.h"
-#include "synch.h"
 
-void SimpleThread(int,Barrier&);
+void SimpleThread(int);
 
 int main( int argc, char * argv[] ) {
-	Barrier b("miBarrera",2);
+	
 	Fork(SimpleThread);
 
 	SimpleThread(1);
@@ -16,25 +15,21 @@ int main( int argc, char * argv[] ) {
 }
 
 
-void SimpleThread(int num, Barrier & b)
+void SimpleThread(int num)
 {
     if (num == 1) {
-		for (num = 0; num < 50; num++) {
-			Write("HOLAA ", 6, 1);
-			//Yield();
-		}
+	for (num = 0; num < 5; num++) {
+		Write("HOLAA\n", 7, 1);
+		Yield();
+	}
     }
 
     else {
 	for (num = 0; num < 5; num++) {
-		Write("2. holaa\n", 10, 1);
-		//Yield();
+		Write("holaa\n", 7, 1);
+		Yield();
 	}
     }
-	Write("LLEGO HILO",10,1);
-	b.Wait();
-	
-	
     Write("Fin de\n", 7, 1);
 }
 
