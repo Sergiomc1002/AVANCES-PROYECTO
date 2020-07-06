f4fbcca387d6c5d269f7fc665299345788bd330f
gustavojh106 <gustavojh106@gmail.com>
2020-07-06 17:35:40 -0600

    isaac se la come

--- proyectos/proyecto/bserver.cpp
+++ proyectos/proyecto/bserver.cpp
@@ -1,3 +1,4 @@
+// Server side implementation of UDP client-server model 
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <unistd.h> 
@@ -6,24 +7,52 @@
 #include <sys/socket.h> 
 #include <arpa/inet.h> 
 #include <netinet/in.h> 
-#include "socket.h"
 
-#define PORT 8080 
+#define PORT	 8080 
 #define MAXLINE 1024 
 
-int main()
-{
-    Socket s('d', false);
-    struct sockaddr_in servaddr, clientaddr;
+// Driver code 
+int main() { 
+	int sockfd; 
+	char buffer[MAXLINE]; 
+	char *hello = "Hello from server"; 
+	struct sockaddr_in servaddr, cliaddr; 
+	
+	// Creating socket file descriptor 
+	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
+		perror("socket creation failed"); 
+		exit(EXIT_FAILURE); 
+	} 
+	
+	memset(&servaddr, 0, sizeof(servaddr)); 
+	memset(&cliaddr, 0, sizeof(cliaddr)); 
+	
+	// Filling server information 
+	servaddr.sin_family = AF_INET; // IPv4 
+	servaddr.sin_addr.s_addr = INADDR_ANY; 
+	servaddr.sin_port = htons(PORT); 
+	
+	// Bind the socket with the server address 
+	if ( bind(sockfd, (const struct sockaddr *)&servaddr, 
+			sizeof(servaddr)) < 0 ) 
+	{ 
+		perror("bind failed"); 
+		exit(EXIT_FAILURE); 
+	} 
+	
+	int n;
+	socklen_t len;
 
-    char * msg = (char *)"Hola cliente";
-    
-    s.Bind(PORT, 0);
+	len = sizeof(cliaddr); //len is value/resuslt 
 
-    char buffer[MAXLINE];
-    int n = s.ReceiveFrom(&clientaddr, buffer, MAXLINE);
-
-    printf("Mensaje recibido: %s\n", buffer);
-
-    return 0;
-}
+	n = recvfrom(sockfd, (char *)buffer, MAXLINE, 
+				MSG_WAITALL, ( struct sockaddr *) &cliaddr, &len); 
+	buffer[n] = '\0'; 
+	printf("Client : %s\n", buffer); 
+	sendto(sockfd, (const char *)hello, strlen(hello), 
+		MSG_CONFIRM, (const struct sockaddr *) &cliaddr, 
+			len); 
+	printf("Hello message sent.\n"); 
+	
+	return 0; 
+} 
